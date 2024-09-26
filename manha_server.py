36df1beae1e11f0c82b827b580578582d5a995cc
Ronak Kumar Samantray <1369203+rokrsa@users.noreply.github.com>
2024-09-26 15:34:34 +0530

    update

--- manha_server.py
+++ manha_server.py
@@ -9,40 +9,13 @@ from bme680 import BME680_I2C
 from adxl345 import ADXL345
 from gps import NeoGPS
 from GPSParser import GPSParser
+from read_sensors import *
 
 # WebSocket server port
 WEBSOCKET_PORT = 8081
 
 # WebSocket GUID for key hashing
 WEBSOCKET_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-
-# Functions to read sensor data
-def read_adxl345(imu):
-    try:
-        return {"x": imu.xValue, "y": imu.yValue, "z": imu.zValue}
-    except Exception as e:
-        print("Failed to read ADXL:", e)
-        
-def read_gps(gps_sensor, gps_parser):
-    try:
-        g_d = gps_sensor.read_gps()
-        for byte in g_d:
-            stat = gps_parser.update(chr(byte))
-            if stat is not None:
-                # return parsed GPS data
-                return {"lat": gps_parser.latitude, "lng": gps_parser.longitude, "alt": gps_parser.altitude, "gps_sc": gps_parser.satellites_in_use, "gps_hdop": gps_parser.hdop }
-            else:
-                return {"lat": 0.0, "lng": 0.0, "alt": -1, "gps_sc": -1, "gps_hdop": -1 }
-    except Exception as e:
-        print(f"Failed to read GPS:", e)
-        
-def read_bme680(bme680):
-    try:
-        return {"temp": bme680.temperature, "humidity": bme680.humidity, "pressure": bme680.pressure, "gas": bme680.gas}
-
-    except Exception as e:
-        print("Error reading bme data:", e)
-        return None
 
 # Manha server class
 class ManhaServer:
