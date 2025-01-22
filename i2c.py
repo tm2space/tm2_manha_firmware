81e68970fcaf192d295d57554d5b74dd544a630e
Gaurav Prajapati <mail2gauravprajapati@gmail.com>
2025-01-22 16:21:57 +0530

    fix pinouts for I2C

--- i2c.py
+++ i2c.py
@@ -1,8 +1,8 @@
 from machine import Pin, SoftI2C
 
 # Pin configuration for I2C communication with the BME680 sensor
-I2C_SCL_PIN = 1  # SCL pin
-I2C_SDA_PIN = 0  # SDA pin
+I2C_SCL_PIN = 19  # SCL pin
+I2C_SDA_PIN = 18  # SDA pin
 
 def init_i2c():
     global m_i2c
