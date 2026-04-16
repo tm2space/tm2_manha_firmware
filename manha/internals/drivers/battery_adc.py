d58614e50351b1f6fc98f4a7ce463ffad74d6ea6
Gaurav Prajapati <gaurav.p@tm2.space>
2026-04-16 16:37:09 +0530

    BatteryADC: use simple Moving Average to reduce noise in ADC readings

--- manha/internals/drivers/battery_adc.py
+++ manha/internals/drivers/battery_adc.py
@@ -1,8 +1,8 @@
 from machine import Pin, ADC
-import time
 
 MAX_BATTERY_VOLTAGE = 4.2
 MIN_BATTERY_VOLTAGE = 3.4
+ADC_BUFFER_SIZE = 10
 
 class BatteryVoltage:
     ADC_VREF = 3.3
@@ -11,13 +11,18 @@ class BatteryVoltage:
 
     def __init__(self, adc_pin=26):
         self.adc_handle = ADC(Pin(adc_pin))
-        
+        self._buf = []
+        self._n = 0
+
     @property
     def rawValue(self) -> int:
-        time.sleep(0.05)  # Allow time for ADC to stabilize
-        _read_val = self.adc_handle.read_u16()
-        return _read_val
-    
+        self._buf.append(self.adc_handle.read_u16())
+        if self._n < ADC_BUFFER_SIZE:
+            self._n += 1
+        else:
+            self._buf.pop(0)
+        return sum(self._buf) // self._n
+
     @property
     def voltage(self) -> float:
         _raw_read_val = self.rawValue
@@ -26,7 +31,7 @@ class BatteryVoltage:
 
         return _read_val_conv
 
-    
+
     @property
     def percValue(self) -> float:
         _voltage = self.voltage
@@ -36,8 +41,8 @@ class BatteryVoltage:
             _voltage = MAX_BATTERY_VOLTAGE
         elif _voltage < MIN_BATTERY_VOLTAGE:
             _voltage = MIN_BATTERY_VOLTAGE
-        
+
         # convert to percentage
         _read_val_conv = (_voltage - MIN_BATTERY_VOLTAGE) / (MAX_BATTERY_VOLTAGE - MIN_BATTERY_VOLTAGE) * 100
-        
+
         return _read_val_conv
